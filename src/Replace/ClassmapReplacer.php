38969f72098b111c1e3b17563f822e4372ef706a
Coen Jacobs <coenjacobs@gmail.com>
2017-03-08 11:15:15 +0100

    Regex magic for classmap replacer

--- src/Replace/ClassmapReplacer.php
+++ src/Replace/ClassmapReplacer.php
@@ -4,8 +4,15 @@ namespace CoenJacobs\Mozart\Replace;
 
 class ClassmapReplacer extends BaseReplacer
 {
-    public function replace( $content )
+    public function replace( $contents )
     {
-        return $content;
+        return preg_replace_callback(
+            '/(?:\W[abstract]*class |interface )([a-zA-Z\_]+)[ ]*{/U',
+            function ($matches) {
+                $replace = 'CJDT_' . $matches[1];
+                return str_replace($matches[1], $replace, $matches[0]);
+            },
+            $contents
+        );
     }
 }
