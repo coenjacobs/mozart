6532826ec721ebc6d9cc913718ca98e348bdb436
Coen Jacobs <coenjacobs@gmail.com>
2017-04-04 09:32:16 +0200

    Basic test that verifies namespace replacement logic

---
+++ tests/replacers/NamespaceReplacerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use CoenJacobs\Mozart\Composer\Autoload\Psr0;
+use CoenJacobs\Mozart\Replace\NamespaceReplacer;
+use PHPUnit\Framework\TestCase;
+
+class NamespaceReplacerTest extends TestCase {
+    /** @var NamespaceReplacer */
+    public $replacer;
+
+    public function setUp()
+    {
+        $autoloader = new Psr0();
+        $autoloader->namespace = 'Test\\Test';
+
+        $replacer = new NamespaceReplacer();
+        $replacer->setAutoloader($autoloader);
+        $replacer->dep_namespace = 'Prefix\\';
+        $this->replacer = $replacer;
+    }
+
+    /** @test */
+    public function it_replaces_namespace_declarations() {
+        $contents = 'namespace Test\\Test;';
+        $contents = $this->replacer->replace($contents);
+        $this->assertEquals('namespace Prefix\\Test\\Test;', $contents);
+    }
+
+    /** @test */
+    public function it_replaces_partial_namespace_declarations() {
+        $contents = 'namespace Test\\Test\\Another;';
+        $contents = $this->replacer->replace($contents);
+        $this->assertEquals('namespace Prefix\\Test\\Test\\Another;', $contents);
+    }
+}
