ab0af7b21443a96ea74aafa85a8da34828279943
Rostislav Wolny <rosta@mailpoet.com>
2018-10-17 17:59:53 +0200

    Add option for processing packages with its sub-dependencies

--- src/Console/Commands/Compose.php
+++ src/Console/Commands/Compose.php
@@ -6,6 +6,7 @@ use CoenJacobs\Mozart\Composer\Package;
 use CoenJacobs\Mozart\Mover;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Compose extends Command
@@ -13,12 +14,20 @@ class Compose extends Command
     protected function configure()
     {
         $this->setName('compose');
+        $this->addOption(
+            'with_dependencies',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Process also sub-dependencies.',
+            false
+        );
         $this->setDescription('Composes all dependencies as a package inside a WordPress plugin.');
         $this->setHelp('');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $with_dependencies = $input->getOption('with_dependencies') !== false;
         $workingDir = getcwd();
 
         $config = json_decode(file_get_contents($workingDir . '/composer.json'));
@@ -27,10 +36,23 @@ class Compose extends Command
         $mover = new Mover($workingDir, $config);
         $mover->deleteTargetDirs();
 
-        foreach ($config->packages as $package_slug) {
+        $packages = new \ArrayIterator($config->packages);
+        $processed_packages = [];
+
+        foreach ($packages as $package_slug) {
             $package = new Package($workingDir . '/vendor/' . $package_slug);
             $package->findAutoloaders();
             $mover->movePackage($package);
+            if (!$with_dependencies) {
+                continue;
+            }
+            $package->findDependencies($workingDir . '/vendor');
+            $processed_packages[] = $package;
+            foreach ($package->dependencies as $dependency) {
+                if (!in_array($dependency, $packages->getArrayCopy())) {
+                    $packages->append($dependency);
+                };
+            }
         }
 
         $mover->replaceClassmapNames();
