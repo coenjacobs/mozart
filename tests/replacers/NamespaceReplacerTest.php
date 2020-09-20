8de962368afcd4088fc30b1defec0e84bc3b7c46
Mark Jaquith <mark@coveredwebservices.com>
2020-09-19 22:51:52 -0400

    Create a more unique default prefix

--- tests/replacers/NamespaceReplacerTest.php
+++ tests/replacers/NamespaceReplacerTest.php
@@ -7,12 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class NamespaceReplacerTest extends TestCase
 {
+    const PREFIX = 'My\\Mozart\\Prefix\\';
+
     /** @var NamespaceReplacer */
     public $replacer;
 
     protected function setUp(): void
     {
-        $this->replacer = self::createReplacer('Test\\Test', 'Prefix\\');
+        $this->replacer = self::createReplacer('Test\\Test');
     }
 
     /**
@@ -23,7 +25,7 @@ class NamespaceReplacerTest extends TestCase
      *
      * @return Psr0
      */
-    protected static function createReplacer(string $namespace, string $prefix) : NamespaceReplacer
+    protected static function createReplacer(string $namespace, string $prefix = self::PREFIX) : NamespaceReplacer
     {
         $autoloader = new Psr0;
         $autoloader->namespace = $namespace;
@@ -40,19 +42,19 @@ class NamespaceReplacerTest extends TestCase
         $contents = 'namespace Test\\Test;';
         $contents = $this->replacer->replace($contents);
 
-        $this->assertEquals('namespace Prefix\\Test\\Test;', $contents);
+        $this->assertEquals('namespace My\\Mozart\\Prefix\\Test\\Test;', $contents);
     }
 
 
     /** @test */
     public function it_doesnt_replaces_namespace_inside_namespace(): void
     {
-        $replacer = self::createReplacer('Test', 'Prefix\\');
+        $replacer = self::createReplacer('Test');
 
         $contents = "namespace Test\\Something;\n\nuse Test\\Test;";
         $contents = $replacer->replace($contents);
 
-        $this->assertEquals("namespace Prefix\\Test\\Something;\n\nuse Prefix\\Test\\Test;", $contents);
+        $this->assertEquals("namespace My\\Mozart\\Prefix\\Test\\Something;\n\nuse My\\Mozart\\Prefix\\Test\\Test;", $contents);
     }
 
     /** @test */
@@ -61,17 +63,17 @@ class NamespaceReplacerTest extends TestCase
         $contents = 'namespace Test\\Test\\Another;';
         $contents = $this->replacer->replace($contents);
 
-        $this->assertEquals('namespace Prefix\\Test\\Test\\Another;', $contents);
+        $this->assertEquals('namespace My\\Mozart\\Prefix\\Test\\Test\\Another;', $contents);
     }
 
 	/** @test */
     public function it_doesnt_prefix_already_prefixed_namespace(): void
     {
-        $replacer = self::createReplacer('Test\\Another', 'Prefix\\');
+        $replacer = self::createReplacer('Test\\Another');
 
-        $contents = 'namespace Prefix\\Test\\Another;';
+        $contents = 'namespace My\\Mozart\\Prefix\\Test\\Another;';
         $contents = $replacer->replace($contents);
 
-        $this->assertEquals('namespace Prefix\\Test\\Another;', $contents);
+        $this->assertEquals('namespace My\\Mozart\\Prefix\\Test\\Another;', $contents);
     }
 }
