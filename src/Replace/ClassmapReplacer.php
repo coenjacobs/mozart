fd5d19485afb283d2d518832b9b986239fca175d
Brian Henry <BrianHenryIE@gmail.com>
2020-12-13 13:01:17 -0800

    Add class comment and multiline comments to regex

--- src/Replace/ClassmapReplacer.php
+++ src/Replace/ClassmapReplacer.php
@@ -1,10 +1,15 @@
 <?php
+/**
+ * The purpose of this file is to find and update classnames (and interfaces...) in their declarations.
+ * Those replaced are recorded and their uses elsewhere are updated in a later step.
+ */
 
 namespace CoenJacobs\Mozart\Replace;
 
 class ClassmapReplacer extends BaseReplacer
 {
-    /** @var array */
+
+    /** @var string[] */
     public $replacedClasses = [];
 
     /** @var string */
@@ -13,7 +18,14 @@ class ClassmapReplacer extends BaseReplacer
     public function replace($contents)
     {
         return preg_replace_callback(
-            '/(?:[abstract]*class |interface )([a-zA-Z0-9_\x7f-\xff]+)\s?(?:\n*|{| extends| implements)/',
+            "
+			/													# Start the pattern 
+						(?:abstract\sclass|class|interface)\s+	# Look behind for class, abstract class, interface
+						([a-zA-Z0-9_\x7f-\xff]+)				# Match the word made of valid classname characters
+						\s?										# Allow a space after
+						(?:{|extends|implements|\n)				# Class declaration can be followed by {, extends, implements, or a new line
+			/x", //  non-greedy matching by default, ignore whitespace in regex.
+
             function ($matches) {
                 $replace = $this->classmap_prefix . $matches[1];
                 $this->saveReplacedClass($matches[1], $replace);
