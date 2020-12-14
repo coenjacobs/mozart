3909dcffb45b8a3b42797f3df0c2a76b687d6498
Brian Henry <BrianHenryIE@gmail.com>
2020-12-13 20:46:12 -0800

    Tidy up code

--- src/Replace/ClassmapReplacer.php
+++ src/Replace/ClassmapReplacer.php
@@ -20,28 +20,27 @@ class ClassmapReplacer extends BaseReplacer
 
         return preg_replace_callback(
             "
-			/													# Start the pattern
-						namespace\s+[a-zA-Z0-9_\x7f-\xff\\\\]+[;{\s\n]{1}.*?(?=namespace|$) 
-																# Look for a preceeding namespace declaration, up until 
-																# a potential second namespace declaration
-						|										# if found, match that much before continuing the search 
-																# on the remainder of the string
-						\s*\\/\\/\s*[^\n]* |					# Skip line comments
-						\s*\\*[^\n]*? |							# Skip multiline comment bodies
-						.*?\\*\\/ |								# Skip multiline comment endings			
-						\s*										# whitespace is allowed before 
-						(?:abstract\sclass|class|interface)\s+	# Look behind for class, abstract class, interface
-						([a-zA-Z0-9_\x7f-\xff]+)				# Match the word until the first 
-																# non-classname-valid character
-						\s?										# Allow a space after
-						(?:{|extends|implements|\n|$)				# Class declaration can be followed by {, extends, 
-																# implements, or a new line
-			/sx", //                                            # dot matches newline, ignore whitespace in regex.
+			/											# Start the pattern
+				namespace\s+[a-zA-Z0-9_\x7f-\xff\\\\]+[;{\s\n]{1}.*?(?=namespace|$) 
+														# Look for a preceeding namespace declaration, up until a 
+														# potential second namespace declaration.
+				|										# if found, match that much before continuing the search on
+														# the remainder of the string.
+				\s*\\/\\/\s*[^\n]* |					# Skip line comments
+				\s*\\*[^\n]*? |							# Skip multiline comment bodies
+				.*?\\*\\/ |								# Skip multiline comment endings			
+				\s*										# Whitespace is allowed before 
+				(?:abstract\sclass|class|interface)\s+	# Look behind for class, abstract class, interface
+				([a-zA-Z0-9_\x7f-\xff]+)				# Match the word until the first non-classname-valid character
+				\s?										# Allow a space after
+				(?:{|extends|implements|\n|$)			# Class declaration can be followed by {, extends, implements 
+														# or a new line
+			/sx", //                                    # dot matches newline, ignore whitespace in regex.
             function ($matches) use ($contents) {
 
                 // If we didn't capture a proper class/interface, return.
-                if( 1 === count( $matches ) ) {
-	                return $matches[0];
+                if (1 === count($matches)) {
+                    return $matches[0];
                 }
 
 
