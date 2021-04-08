2709af127454113a45d0a0cf5741e83848adfeda
Brian Henry <BrianHenryIE@gmail.com>
2021-04-08 10:55:34 -0700

    Add integration tests TestCase subclass

---
+++ tests/Util/IntegrationTestCase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CoenJacobs\Mozart\Util;
+
+use PHPUnit\Framework\TestCase;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class IntegrationTestCase extends TestCase {
+
+    protected $testsWorkingDir;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->testsWorkingDir = __DIR__ . '/temptestdir/';
+
+        if (file_exists($this->testsWorkingDir)) {
+            $this->deleteDir( $this->testsWorkingDir );
+        }
+
+        @mkdir($this->testsWorkingDir, );
+    }
+
+
+    /**
+     * Delete $this->testsWorkingDir after each test.
+     *
+     * @see https://stackoverflow.com/questions/3349753/delete-directory-with-files-in-it
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $dir = $this->testsWorkingDir;
+
+        $this->deleteDir($dir);
+    }
+
+    protected function deleteDir($dir)
+    {
+        if (!file_exists($dir)) {
+            return;
+        }
+
+        $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+        rmdir($dir);
+    }
+
+}
