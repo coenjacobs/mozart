2a3a67bc6fec78c3dbbead1679d2aa4c5846d0cb
Brian Henry <BrianHenryIE@gmail.com>
2021-05-25 23:24:58 -0700

    Delete GitHub Actions generated files

--- pcov.sh
+++
@@ -1,7 +0,0 @@
-git clone --depth=1 https://github.com/krakjoe/pcov.git
-(
-cd pcov && phpize
-./configure --enable-pcov
-make
-sudo make install
-)
--- xdebug.sh
+++
@@ -1,5 +0,0 @@
-git clone --depth=1 https://github.com/xdebug/xdebug.git
-(
-  cd xdebug || echo "cd failed"
-  sudo ./rebuild.sh
-)
