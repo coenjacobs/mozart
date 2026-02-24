coenjacobs/mozart

commit be0ba40d797c6763729418f8578fdd7f5f1b6b24

Reduce Mover complexity by defering file path handling to autoloaders

src/Composer/Autoload/NamespaceAutoloader.php
@@ -2,6 +2,9 @@
 
 namespace CoenJacobs\Mozart\Composer\Autoload;
 
+use CoenJacobs\Mozart\FilesHandler;
+use Symfony\Component\Finder\SplFileInfo;
+
 abstract class NamespaceAutoloader extends AbstractAutoloader
 {
     /** @var string */
@@ -15,6 +18,8 @@ abstract class NamespaceAutoloader extends AbstractAutoloader
      * @var array<string>
      */
     public $paths = [];
+
+    private FilesHandler $fileHandler;
 
     /**
      * A package's composer.json config autoload key's value, where $key is `psr-1`|`psr-4`|`classmap`.
@@ -46,5 +51,51 @@ abstract class NamespaceAutoloader extends AbstractAutoloader
     public function getNamespacePath(): string
     {
         return '';
+    }
+
+    public function getFiles(FilesHandler $fileHandler): array
+    {
+        $this->fileHandler = $fileHandler;
+        $filesToMove = array();
+
+        foreach ($this->paths as $path) {
+            $sourcePath = $fileHandler->getConfig()->getWorkingDir() . 'vendor' . DIRECTORY_SEPARATOR
+                        . $this->getPackage()->getName() . DIRECTORY_SEPARATOR . $path;
+
+            $sourcePath = str_replace('/', DIRECTORY_SEPARATOR, $sourcePath);
+
+
+            $files = $fileHandler->getFilesFromPath($sourcePath);
+
+            foreach ($files as $foundFile) {
+                $filePath = $foundFile->getRealPath();
+                $filesToMove[ $filePath ] = $foundFile;
+            }
+        }
+
+        return $filesToMove;
+    }
+
+    public function getTargetFilePath(SplFileInfo $file): string
+    {
+        $suffix = '';
+        foreach ($this->paths as $path) {
+            if (! empty(strstr($file->getPathname(), $this->getPackage()->getName() . DIRECTORY_SEPARATOR . $path))) {
+                $suffix = $path;
+                break;
+            }
+        }
+
+        $replaceWith = $this->fileHandler->getConfig()->getDepDirectory() . $this->getNamespacePath();
+        $targetFile = str_replace($this->fileHandler->getConfig()->getWorkingDir(), $replaceWith, $file->getPathname());
+
+        $packageVendorPath = DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . $this->getPackage()->getName();
+
+        if (! empty($suffix)) {
+            $packageVendorPath = $packageVendorPath . DIRECTORY_SEPARATOR . $suffix;
+        }
+
+        $packageVendorPath = str_replace('/', DIRECTORY_SEPARATOR, $packageVendorPath);
+        return str_replace($packageVendorPath, DIRECTORY_SEPARATOR, $targetFile);
     }
 }

src/Config/Classmap.php
@@ -3,7 +3,9 @@
 namespace CoenJacobs\Mozart\Config;
 
 use CoenJacobs\Mozart\Composer\Autoload\AbstractAutoloader;
+use CoenJacobs\Mozart\FilesHandler;
 use Exception;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Classmap extends AbstractAutoloader
 {
@@ -12,6 +14,8 @@ class Classmap extends AbstractAutoloader
 
     /** @var string[] */
     public $paths = [];
+
+    private FilesHandler $fileHandler;
 
     /**
      * @inheritdoc
@@ -34,5 +38,65 @@ class Classmap extends AbstractAutoloader
     public function getSearchNamespace(): string
     {
         throw new Exception('Classmap autoloaders do not contain a namespace and this method can not be used.');
+    }
+
+    /**
+     * @return array<string,SplFileInfo>
+     */
+    public function getFiles(FilesHandler $fileHandler): array
+    {
+        $this->fileHandler = $fileHandler;
+        $filesToMove = array();
+
+        foreach ($this->files as $file) {
+            $sourcePath = $fileHandler->getConfig()->getWorkingDir() . 'vendor'
+                            . DIRECTORY_SEPARATOR . $this->getPackage()->getName();
+
+            $files = $fileHandler->getFile($sourcePath, $file);
+
+            foreach ($files as $foundFile) {
+                $filePath = $foundFile->getRealPath();
+                $filesToMove[ $filePath ] = $foundFile;
+            }
+        }
+
+        foreach ($this->paths as $path) {
+            $sourcePath = $fileHandler->getConfig()->getWorkingDir() . 'vendor'
+                            . DIRECTORY_SEPARATOR . $this->getPackage()->getName() . DIRECTORY_SEPARATOR . $path;
+
+            $files = $fileHandler->getFilesFromPath($sourcePath);
+            foreach ($files as $foundFile) {
+                $filePath = $foundFile->getRealPath();
+                $filesToMove[ $filePath ] = $foundFile;
+            }
+        }
+
+        return $filesToMove;
+    }
+
+    public function getTargetFilePath(SplFileInfo $file): string
+    {
+        $suffix = '';
+        foreach ($this->paths as $path) {
+            if (! empty(strstr($file->getPathname(), $this->getPackage()->getName() . DIRECTORY_SEPARATOR . $path))) {
+                $suffix = $path;
+                break;
+            }
+        }
+
+        $namespacePath = $this->getPackage()->getName();
+        $replaceWith = $this->fileHandler->getConfig()->getClassmapDirectory() . $namespacePath . DIRECTORY_SEPARATOR;
+
+        $targetFile = str_replace($this->fileHandler->getConfig()->getWorkingDir(), $replaceWith, $file->getPathname());
+
+        $packageVendorPath = DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . $this->getPackage()->getName()
+                                . DIRECTORY_SEPARATOR;
+
+        if (! empty($suffix)) {
+            $packageVendorPath = $packageVendorPath . DIRECTORY_SEPARATOR . $suffix;
+        }
+
+        $packageVendorPath = str_replace('/', DIRECTORY_SEPARATOR, $packageVendorPath);
+        return str_replace($packageVendorPath, DIRECTORY_SEPARATOR, $targetFile);
     }
 }

src/Config/Mozart.php
@@ -105,12 +105,12 @@ class Mozart
      */
     public function getDepDirectory(): string
     {
-        return rtrim($this->depDirectory, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+        return trim($this->depDirectory, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
     }
 
     public function getClassmapDirectory(): string
     {
-        return rtrim($this->classmapDir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+        return trim($this->classmapDir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
     }
 
     public function getDeleteVendorDirectories(): bool
@@ -154,6 +154,6 @@ class Mozart
 
     public function getWorkingDir(): string
     {
-        return $this->workingDir;
+        return rtrim($this->workingDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
     }
 }

src/Composer/Autoload/AbstractAutoloader.php
@@ -2,8 +2,22 @@
 
 namespace CoenJacobs\Mozart\Composer\Autoload;
 
+use CoenJacobs\Mozart\Config\Package;
+
 abstract class AbstractAutoloader implements Autoloader
 {
+    private Package $package;
+
+    public function getPackage(): Package
+    {
+        return $this->package;
+    }
+
+    public function setPackage(Package $package): void
+    {
+        $this->package = $package;
+    }
+
     public function getOutputDir(string $basePath, string $autoloadPath): string
     {
         $outputDir = $basePath . $autoloadPath;

src/Composer/Autoload/Autoloader.php
@@ -2,6 +2,9 @@
 
 namespace CoenJacobs\Mozart\Composer\Autoload;
 
+use CoenJacobs\Mozart\FilesHandler;
+use Symfony\Component\Finder\SplFileInfo;
+
 interface Autoloader
 {
     /**
@@ -10,4 +13,9 @@ interface Autoloader
     public function processConfig($autoloadConfig): void;
     public function getSearchNamespace(): string;
     public function getOutputDir(string $basePath, string $autoloadPath): string;
+    /**
+     * @return array<string,SplFileInfo>
+     */
+    public function getFiles(FilesHandler $files): array;
+    public function getTargetFilePath(SplFileInfo $file): string;
 }

src/Config/Autoload.php
@@ -10,7 +10,7 @@ class Autoload
     /** @var array<Autoloader> */
     public array $autoloaders = [];
 
-    public function setupAutoloaders(stdClass $autoloadData): void
+    public function setupAutoloaders(stdClass $autoloadData, Package $package): void
     {
         $autoloaders = [];
 
@@ -20,6 +20,7 @@ class Autoload
                 $autoloader = new Psr4();
                 $autoloader->namespace = $key;
                 $autoloader->processConfig($value);
+                $autoloader->setPackage($package);
                 $autoloaders[] = $autoloader;
             }
         }
@@ -30,6 +31,7 @@ class Autoload
                 $autoloader = new Psr0();
                 $autoloader->namespace = $key;
                 $autoloader->processConfig($value);
+                $autoloader->setPackage($package);
                 $autoloaders[] = $autoloader;
             }
         }
@@ -37,6 +39,7 @@ class Autoload
         if (isset($autoloadData->classmap)) {
             $autoloader = new Classmap();
             $autoloader->processConfig($autoloadData->classmap);
+            $autoloader->setPackage($package);
             $autoloaders[] = $autoloader;
         }
 

src/Config/Package.php
@@ -25,10 +25,12 @@ class Package
     public ?Autoload $autoload = null;
     public ?Extra $extra = null;
 
+    private bool $dependenciesLoaded = false;
+
     public function setAutoload(stdClass $data): void
     {
         $autoload = new Autoload();
-        $autoload->setupAutoloaders($data);
+        $autoload->setupAutoloaders($data, $this);
         $this->autoload = $autoload;
     }
 
@@ -85,6 +87,10 @@ class Package
 
     public function loadDependencies(PackageFinder $finder): void
     {
+        if ($this->dependenciesLoaded) {
+            return;
+        }
+
         if ($this->isValidMozartConfig() && !empty($this->getExtra())) {
             $mozart = $this->getExtra()->getMozart();
 
@@ -97,6 +103,7 @@ class Package
         $dependencies = $finder->getPackagesBySlugs($this->getRequire());
 
         $this->registerDependencies($dependencies);
+        $this->dependenciesLoaded = true;
     }
 
     public function registerDependency(Package $package): void

src/Console/Commands/Compose.php
@@ -78,12 +78,10 @@ class Compose extends Command
         $finder->setConfig($this->config);
 
         $package->loadDependencies($finder);
+        $packages = $finder->findPackages($package->getDependencies());
 
-        $packages = $finder->getPackagesBySlugs($require);
-        $packages = $finder->findPackages($packages);
-
-        $this->mover = new Mover($this->workingDir, $this->config);
-        $this->replacer = new Replacer($this->workingDir, $this->config);
+        $this->mover = new Mover($this->config);
+        $this->replacer = new Replacer($this->config);
 
         $this->mover->deleteTargetDirs($packages);
         $this->mover->movePackages($packages);

commit e1acb8617b428323ad6437e90abb423e40676850

Fix Windows directory separator breaking PSR-4 path flattening
Normalize forward slashes to DIRECTORY_SEPARATOR in path comparisons
and config getters. On Windows, config values use forward slashes
(from composer.json) but SplFileInfo::getPathname() returns backslash
paths. This mismatch caused str_contains() suffix detection to fail,
leaving the autoloader source directory (e.g. src/) in the output path.

Regression from be0ba40 which moved path detection into autoloaders
using str_contains() instead of receiving the path as a parameter.

Fixes #307

src/Composer/Autoload/NamespaceAutoloader.php
@@ -94,7 +94,8 @@ abstract class NamespaceAutoloader extends AbstractAutoloader
         $suffix = '';
         foreach ($this->paths as $path) {
             $directoryName = $this->getPackage()->getDirectoryName();
-            if (str_contains($file->getPathname(), $directoryName . DIRECTORY_SEPARATOR . $path)) {
+            $searchPath = str_replace('/', DIRECTORY_SEPARATOR, $directoryName . DIRECTORY_SEPARATOR . $path);
+            if (str_contains($file->getPathname(), $searchPath)) {
                 $suffix = $path;
                 break;
             }

src/Config/Classmap.php
@@ -89,7 +89,8 @@ class Classmap extends AbstractAutoloader
         $suffix = '';
         foreach ($this->paths as $path) {
             $directoryName = $this->getPackage()->getDirectoryName();
-            if (str_contains($file->getPathname(), $directoryName . DIRECTORY_SEPARATOR . $path)) {
+            $searchPath = str_replace('/', DIRECTORY_SEPARATOR, $directoryName . DIRECTORY_SEPARATOR . $path);
+            if (str_contains($file->getPathname(), $searchPath)) {
                 $suffix = $path;
                 break;
             }

src/Config/Files.php
@@ -117,7 +117,7 @@ class Files extends AbstractAutoloader
         }
 
         // File is global scope - put it in classmap_directory with package name
-        $packageName = $this->getPackage()->getDirectoryName();
+        $packageName = str_replace('/', DIRECTORY_SEPARATOR, $this->getPackage()->getDirectoryName());
         return $this->fileHandler->getConfig()->getClassmapDirectory()
             . $packageName . DIRECTORY_SEPARATOR . $file->getFilename();
     }

src/Config/Mozart.php
@@ -110,12 +110,14 @@ class Mozart
      */
     public function getDepDirectory(): string
     {
-        return trim($this->depDirectory, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $dir = str_replace('/', DIRECTORY_SEPARATOR, $this->depDirectory);
+        return trim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
     }
 
     public function getClassmapDirectory(): string
     {
-        return trim($this->classmapDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $dir = str_replace('/', DIRECTORY_SEPARATOR, $this->classmapDir);
+        return trim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
     }
 
     public function getDeleteVendorDirectories(): bool

tests/Unit/Autoload/NamespaceAutoloaderTargetFileTest.php
@@ -1,0 +1,193 @@
+<?php
+
+namespace CoenJacobs\Mozart\Tests\Unit\Autoload;
+
+use CoenJacobs\Mozart\Config\Mozart;
+use CoenJacobs\Mozart\Config\Package;
+use CoenJacobs\Mozart\Config\Psr4;
+use CoenJacobs\Mozart\FilesHandler;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Finder\SplFileInfo;
+
+class NamespaceAutoloaderTargetFileTest extends TestCase
+{
+    private string $testDir;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->testDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'mozart_autoloader_test_' . uniqid();
+        mkdir($this->testDir, 0777, true);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        if (is_dir($this->testDir)) {
+            $this->removeDirectory($this->testDir);
+        }
+    }
+
+    /** @test */
+    public function it_strips_src_path_from_psr4_target_file(): void
+    {
+        // Set up directory structure: vendor/php-di/php-di/src/Attribute/Inject.php
+        $vendorDir = $this->testDir . DIRECTORY_SEPARATOR . 'vendor'
+            . DIRECTORY_SEPARATOR . 'php-di' . DIRECTORY_SEPARATOR . 'php-di'
+            . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Attribute';
+        mkdir($vendorDir, 0777, true);
+        file_put_contents($vendorDir . DIRECTORY_SEPARATOR . 'Inject.php', '<?php' . PHP_EOL);
+
+        $config = new Mozart();
+        $config->setDepDirectory('src/Dependencies/');
+        $config->setDepNamespace('MyPlugin\\Dependencies\\');
+        $config->setClassmapDirectory('src/Dependencies/classmap/');
+        $config->setClassmapPrefix('MyPlugin_');
+        $config->setWorkingDir($this->testDir . DIRECTORY_SEPARATOR);
+
+        $package = new Package();
+        $package->name = 'php-di/php-di';
+
+        $autoloader = new Psr4();
+        $autoloader->setPackage($package);
+        $autoloader->setNamespace('DI\\');
+        $autoloader->processConfig('src/');
+
+        $fileHandler = new FilesHandler($config);
+        $files = $autoloader->getFiles($fileHandler);
+
+        $this->assertNotEmpty($files, 'Should find files in vendor directory');
+
+        $file = reset($files);
+        $targetPath = $autoloader->getTargetFilePath($file);
+
+        // The src/ directory should be stripped — the target path should NOT contain /src/
+        $this->assertStringNotContainsString(
+            DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Attribute',
+            $targetPath,
+            'The src/ path should be stripped from the target file path'
+        );
+
+        // Should end with the namespace-based path
+        $this->assertStringEndsWith(
+            DIRECTORY_SEPARATOR . 'DI' . DIRECTORY_SEPARATOR . 'Attribute' . DIRECTORY_SEPARATOR . 'Inject.php',
+            $targetPath
+        );
+    }
+
+    /** @test */
+    public function it_handles_empty_psr4_path(): void
+    {
+        // Set up directory structure: vendor/some/package/Foo/Bar.php
+        $vendorDir = $this->testDir . DIRECTORY_SEPARATOR . 'vendor'
+            . DIRECTORY_SEPARATOR . 'some' . DIRECTORY_SEPARATOR . 'package'
+            . DIRECTORY_SEPARATOR . 'Foo';
+        mkdir($vendorDir, 0777, true);
+        file_put_contents($vendorDir . DIRECTORY_SEPARATOR . 'Bar.php', '<?php' . PHP_EOL);
+
+        $config = new Mozart();
+        $config->setDepDirectory('lib/Dependencies/');
+        $config->setDepNamespace('Plugin\\Deps\\');
+        $config->setClassmapDirectory('lib/Dependencies/classmap/');
+        $config->setClassmapPrefix('Plugin_');
+        $config->setWorkingDir($this->testDir . DIRECTORY_SEPARATOR);
+
+        $package = new Package();
+        $package->name = 'some/package';
+
+        $autoloader = new Psr4();
+        $autoloader->setPackage($package);
+        $autoloader->setNamespace('Some\\Namespace\\');
+        $autoloader->processConfig('');
+
+        $fileHandler = new FilesHandler($config);
+        $files = $autoloader->getFiles($fileHandler);
+
+        $this->assertNotEmpty($files, 'Should find files in vendor directory');
+
+        $file = reset($files);
+        $targetPath = $autoloader->getTargetFilePath($file);
+
+        // Vendor path components should not be in the target
+        $this->assertStringNotContainsString('vendor', $targetPath);
+        $this->assertStringNotContainsString('some' . DIRECTORY_SEPARATOR . 'package', $targetPath);
+
+        // Should contain namespace path and the file
+        $this->assertStringContainsString(
+            'Some' . DIRECTORY_SEPARATOR . 'Namespace',
+            $targetPath
+        );
+        $this->assertStringEndsWith('Bar.php', $targetPath);
+    }
+
+    /** @test */
+    public function it_strips_path_with_array_of_psr4_paths(): void
+    {
+        // Set up directory structure with two source paths
+        $srcDir = $this->testDir . DIRECTORY_SEPARATOR . 'vendor'
+            . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'multi'
+            . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Core';
+        mkdir($srcDir, 0777, true);
+        file_put_contents($srcDir . DIRECTORY_SEPARATOR . 'Engine.php', '<?php' . PHP_EOL);
+
+        $libDir = $this->testDir . DIRECTORY_SEPARATOR . 'vendor'
+            . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'multi'
+            . DIRECTORY_SEPARATOR . 'lib';
+        mkdir($libDir, 0777, true);
+        file_put_contents($libDir . DIRECTORY_SEPARATOR . 'Helper.php', '<?php' . PHP_EOL);
+
+        $config = new Mozart();
+        $config->setDepDirectory('deps/');
+        $config->setDepNamespace('App\\Deps\\');
+        $config->setClassmapDirectory('deps/classmap/');
+        $config->setClassmapPrefix('App_');
+        $config->setWorkingDir($this->testDir . DIRECTORY_SEPARATOR);
+
+        $package = new Package();
+        $package->name = 'test/multi';
+
+        $autoloader = new Psr4();
+        $autoloader->setPackage($package);
+        $autoloader->setNamespace('Test\\Multi\\');
+        $autoloader->processConfig(['src/', 'lib/']);
+
+        $fileHandler = new FilesHandler($config);
+        $files = $autoloader->getFiles($fileHandler);
+
+        $this->assertCount(2, $files, 'Should find files from both source paths');
+
+        foreach ($files as $file) {
+            $targetPath = $autoloader->getTargetFilePath($file);
+
+            // Neither src/ nor lib/ should remain in the target path
+            $this->assertStringNotContainsString(
+                DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Core',
+                $targetPath,
+                'The src/ path should be stripped from target: ' . $targetPath
+            );
+            $this->assertStringNotContainsString(
+                DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Helper',
+                $targetPath,
+                'The lib/ path should be stripped from target: ' . $targetPath
+            );
+        }
+    }
+
+    /**
+     * @param string $dir
+     */
+    private function removeDirectory(string $dir): void
+    {
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $files = array_diff(scandir($dir), ['.', '..']);
+        foreach ($files as $file) {
+            $path = $dir . DIRECTORY_SEPARATOR . $file;
+            is_dir($path) ? $this->removeDirectory($path) : unlink($path);
+        }
+
+        rmdir($dir);
+    }
+}